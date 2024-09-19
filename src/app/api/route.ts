58246a004bbe7f5094f263016c61bc3302509583
hechima256 <hechima11444@gmail.com>
2024-09-19 21:38:06 +0900

    APIのデータ取得処理を作成

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,8 +1,35 @@
-export async function GET(request: Request) {
-	const message = {
-		hoge: 0,
-		fuga: "hello",
-	};
+import { PixelArt } from "@/models/PixelArt";
+import connectDB from "@/utils/mongodb";
+import { NextResponse } from "next/server";
 
-	return Response.json({ message });
+export async function GET(request: Request) {
+	try {
+		await connectDB();
+		const pixelArt = await PixelArt.findOne().lean();
+		if (!pixelArt) {
+			return NextResponse.json(
+				{ status: "error", message: "絵が見つかりません" },
+				{ status: 404 },
+			);
+		}
+
+		const len = pixelArt.colorHistory.length;
+		const lastColor = len > 0 ? pixelArt.colorHistory[len - 1] : null;
+		delete lastColor?._id; // propsに渡すために除外
+		if (!lastColor) {
+			return NextResponse.json(
+				{ status: "error", message: "絵の履歴がありません" },
+				{ status: 404 },
+			);
+		}
+
+		return NextResponse.json({ status: "success", data: lastColor });
+	} catch (error) {
+		console.error("Error in GET request:", error);
+
+		return NextResponse.json(
+			{ status: "error", message: "サーバーエラーが発生しました" },
+			{ status: 500 },
+		);
+	}
 }
